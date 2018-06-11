2555e7475d507d40cac0da215459a918071de7e2
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-11 22:07:29 +0530

    Create be_s1_pro6.java

---
+++ be_s1_pro6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class G_beginner_1_6
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+          if(s%4==0)
+             System.out.print("yes");
+          else
+             System.out.print("not");
+    }
+}
