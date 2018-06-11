ecd255403d643681796b33f77d97a92914e5d007
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-11 21:46:04 +0530

    Create pro1.java

---
+++ pro1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class G_beginner_1_1
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0)
+          System.out.print("Positive");
+          else if(n==0)
+          System.out.print("Zero");
+          else 
+          System.out.print("Negative");
+          
+    }
+}
