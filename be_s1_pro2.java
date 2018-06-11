c86c2f8ce96b99ad84dee47a7dea8be65931114f
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-11 21:56:31 +0530

    Create be_s1_pro2.java

---
+++ be_s1_pro2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class G_beginner_1_2
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+          System.out.print("Even");
+          else 
+          System.out.print("Odd");
+    }
+}
