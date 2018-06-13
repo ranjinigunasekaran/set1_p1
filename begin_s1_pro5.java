46b26c37cf861712b62071605a7f5c073e733e7e
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-13 22:13:09 +0530

    Create begin_s1_pro5.java

---
+++ begin_s1_pro5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class G_beginner_2_5
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        for(int i=a+1;i<b;i++)
+         if(i%2==0)
+            System.out.print(i+" ");
+    }
+}
