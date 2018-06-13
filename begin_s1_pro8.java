8ff4c11fc37b35cffe3cad547914e45619e98e98
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-13 21:44:30 +0530

    Create begin_s1_pro8.java

---
+++ begin_s1_pro8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class G_beginner_1_8
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int sum=0;
+         for(int i=0;i<=s;i++)
+             sum=sum+i;
+        System.out.print(sum);
+    }
+}
