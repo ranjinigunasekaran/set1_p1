b1287140c073c81c9a559fc965e0434151c7578f
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-13 22:03:16 +0530

    Create begin_s2_pro1.java

---
+++ begin_s2_pro1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class G_beginner_2_1
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double big=Math.pow(a,b);
+        System.out.print((int)big);
+    }
+}
