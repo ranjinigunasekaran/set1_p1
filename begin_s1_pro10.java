6aac5389b1e865387622fcda5078d6dac379643c
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-13 22:00:25 +0530

    Create begin_s1_pro10.java

---
+++ begin_s1_pro10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class G_beginner_1_10
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int count=Integer.toString(N).length();
+        System.out.print(count);
+    }
+}
