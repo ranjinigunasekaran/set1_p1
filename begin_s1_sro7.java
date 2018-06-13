5ad65db6f5ab80b53647e50170e624fbdf654327
ranjinigunasekaran <40069348+ranjinigunasekaran@users.noreply.github.com>
2018-06-13 21:42:08 +0530

    Create begin_s1_sro7.java

---
+++ begin_s1_sro7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class G_beginner_1_7
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+         for(int i=0;i<s;i++)
+             System.out.println("Hello");
+    }
+}
